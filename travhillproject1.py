ca1706f3238f00fb4b514f0463a60cc6c64b67d8
Travis <t4hill@student.bridgew.edu>
2022-10-13 12:42:10 -0400

    WOOOOOOOOO

---
+++ travhillproject1.py
@@ -0,0 +1,29 @@
+student1 = ["00102342", "John S", "GPA 3.2"]
+student2 = ["00323123", "Enping L", "GPA 3.9"]
+student3 = ["00301110", "Margaret B", "GPA 3.3"]
+student4 = ["00205323", "Seikyung J", "GPA 3.7"]
+student5 = ["00432529", "Haleh K", "GPA 3.5"]
+student6 = ["00499823", "Paul K", "GPA 3.8"]
+student7 = ["00010002", "Abdul S", "GPA 3.2"]
+print(student1[0])
+print(student1[2])
+print(student2[0])
+print(student2[2])
+print(student3[0])
+print(student3[2])
+print(student4[0])
+print(student4[2])
+print(student5[0])
+print(student5[2])
+print(student6[0])
+print(student6[2])
+print(student7[0])
+print(student7[2])
+
+
+
+
+
+
+
+#studentname = ["John S, Enping L, Margaret B, Seikyung J, Haleh K, Paul K, Abdul S"]
